db22f4f718b8fc4e6544b100349b13a3e7235623
Laete Meireles <laetemn@gmail.com>
2019-11-24 10:51:21 -0300

    About designer

--- About.Designer.cs
+++ About.Designer.cs
@@ -67,6 +67,7 @@
             // logoPictureBox
             // 
             this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoPictureBox.Image = global::PSX2_CNF_Creator.Properties.Resources.PSX2cnfcreator_logo;
             this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
             this.logoPictureBox.Name = "logoPictureBox";
             this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
